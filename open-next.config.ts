922a06b858347cfa95791167abe989fb61a70c0f
Juul <>
2025-03-27 13:15:30 +0100

    feat: update to open-next 0.6.2

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,6 +1,6 @@
 import type { OpenNextConfig } from '@opennextjs/aws/types/open-next.js';
 import { defineCloudflareConfig } from '@opennextjs/cloudflare';
-import kvIncrementalCache from '@opennextjs/cloudflare/kv-cache';
+import kvIncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache";
 
 const config: OpenNextConfig = {
   ...defineCloudflareConfig({
